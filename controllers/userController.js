23996edfed033b1cd0197c1492ef4857a76aa9a8
heidiwu3388 <heidiwu3388@yahoo.com>
2023-02-07 23:02:42 -0800

    work on getUsers(), createUser() and getSingleUser()

--- controllers/userController.js
+++ controllers/userController.js
@@ -4,27 +4,43 @@ const { User, Thought } = require('../models');
 // Get all users 
 // GET /api/users/
 function getUsers(req, res) {
-  res.json("Coming Soon")
-}
-
-// Create a user 
-// POST /api/users/
-/* example data:
-{
-  "username": "lernantino",
-  "email": "lernantino@gmail.com"
-}
-*/
-function createUser(req, res) {
-  res.json("Coming Soon")
+  User.find({})
+    .select("-__v")
+    .then((users) => res.json(users))
+    .catch((err) => res.status(500).json(err))
+  }
+  
+  // Create a user 
+  // POST /api/users/
+  /* example data:
+  {
+    "username": "lernantino",
+    "email": "lernantino@gmail.com"
+  }
+  */
+ function createUser(req, res) {
+  User.create(req.body)
+    .select("-__v")
+    .then((user) => res.json(user))
+    .catch((err) => res.status(500).json(err));
 }
 
 // Get a single user
 // GET /api/users/:userId
 function getSingleUser(req, res) {
-  res.json("Coming Soon")
+  User.findOne({ _id: req.params.userId })
+    .select("-__v")
+    .populate({ path: 'thoughts', select: '-__v' })
+    .populate({ path: 'friends', select: '-__v' })
+    .then((user) => {
+      if (user) {
+        res.json(user);
+      } else {
+        res.status(404).json(`No user with id = ${req.params.userId}`);
+      }
+    })
+    .catch((err) => res.status(500).json(err));
 }
-
 // Update user
 // PUT /api/users/:userId
 function updateUser(req, res) {

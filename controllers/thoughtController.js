e2eecd4196dbc44b9371c128b9551440457324be
heidiwu3388 <heidiwu3388@yahoo.com>
2023-02-10 13:51:39 -0800

    work on getThoughts() and createThought()

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -1,9 +1,12 @@
-const { thought, Thought } = require('../models');
+const { thought, Thought, User } = require('../models');
 
 // Get all thoughts 
 // GET /api/thoughts/
 function getThoughts(req, res) {
-  res.json("Coming Soon")
+  Thought.find({})
+    .select("-__v")
+    .then((thoughts) => res.status(200).json(thoughts))
+    .catch((err) => res.status(500).json(err));
 }
 
 // Create a thought 
@@ -11,12 +14,36 @@ function getThoughts(req, res) {
 /* example data:
 {
   "thoughtText": "Here's a cool thought...",
-  "thoughtname": "lernantino",
-  "thoughtId": "5edff358a0fcb779aa7b118b"
+  "username": "lernantino",
+  "userId": "5edff358a0fcb779aa7b118b"
 }
 */
 function createThought(req, res) {
-  res.json("Coming Soon")
+  User.findById(req.body.userId) //check if the user exists
+    .then((user) => {
+      if (!user) { // if no user is found, send 404
+        res.status(404).json( `User id not found! (${req.body.userId})` );
+        return;
+      } 
+      // create a new thought
+      Thought.create(req.body)
+        .then((thought) => {  
+          // add the thought id to the user's thought list
+          return User.findOneAndUpdate(
+              { _id: req.body.userId },
+              { $addToSet: { thoughts: thought._id } },
+              { new: true }
+            )
+          .select("-__v")
+          .populate({ path: "thoughts", select: "-__v" });
+        })
+        .then((user) => {
+          // return the updated user document
+          res.status(200).json(user);
+        })
+        .catch((err) => res.status(500).json(err));
+    })
+    .catch((err) => res.status(500).json(err));
 }
 
 // Get a single thought
